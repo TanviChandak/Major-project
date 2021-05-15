f04649aa15bb8f18daa6e7ff4e8c07639e8530d4
Tanvi <tanvichandak18@gmail.com>
2021-05-16 01:29:27 +0530

    tanvi

---
+++ Script.js
@@ -0,0 +1,114 @@
+let canvas = document.querySelector("canvas")
+canvas.height = window.innerHeight
+canvas.width= window.innerWidth/1.19
+let tool = canvas.getContext("2d");
+
+
+let isMouseDown = false
+tool.strokeStyle="#800000"
+
+// tool.fillStyle="black"
+// tool.fillRect(0,0,canvas.height/2,canvas.width/2)
+canvas.addEventListener("mousedown",function(e){
+    tool.beginPath()
+    isMouseDown=true
+    tool.moveTo(e.clientX,getCoordinatesy(e.clientY))
+})
+canvas.addEventListener("mouseup",function(e){
+    isMouseDown=false
+})
+canvas.addEventListener("mousemove",function(e){
+    if(isMouseDown){
+        tool.lineTo(e.clientX,getCoordinatesy(e.clientY))
+        tool.stroke()
+    }
+})
+canvas.addEventListener("touchstart",function(e){
+    e.preventDefault()
+    tool.beginPath()
+    isMouseDown=true
+    tool.moveTo(e.touches[0].clientX,getCoordinatesy(e.touches[0].clientY))
+})
+canvas.addEventListener("touchend",function(e){
+    e.preventDefault()
+    isMouseDown=false
+})
+canvas.addEventListener("touchmove",function(e){
+    if(isMouseDown){
+        e.preventDefault()
+        tool.lineTo(e.touches[0].clientX,getCoordinatesy(e.touches[0].clientY))
+        tool.stroke()
+    }
+})
+let options = document.querySelectorAll(".tool")
+for(let i =0;i< options.length;i++){
+    options[i].addEventListener("click",function(e){
+        console.log("hhh")
+        let option = e.currentTarget
+        let name = option.getAttribute("id")
+        if (name == "eraser"){
+            tool.strokeStyle = document.getElementById("colorpicker2").value;
+        }else if(name == "pencil"){
+            tool.strokeStyle= document.getElementById("colorpicker").value;
+        }else if(name =="black1"){
+            tool.lineWidth=0.5
+        }
+        else if(name =="black2"){
+            tool.lineWidth=7
+        }
+        else if(name =="black3"){
+            tool.lineWidth=20
+        }else if(name =="colorpicker"){
+            var backRGB = document.getElementById("colorpicker").value;
+            document.getElementById("colorpicker").onchange = function() {
+            backRGB = this.value
+            tool.strokeStyle=backRGB
+            }
+        }else if(name =="colorpicker2"){
+            var backRGB = document.getElementById("colorpicker2").value;
+            document.getElementById("colorpicker2").onchange = function() {
+            backRGB = this.value
+            console.log(backRGB)
+            tool.fillStyle = backRGB
+            tool.fillRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    })
+}
+function getCoordinatesy(y){
+    let bounds = canvas.getBoundingClientRect()
+    return y-bounds.y 
+}
+
+var history = {
+    redo_list: [],
+    undo_list: [],
+    saveState: function(canvas, list, keep_redo) {
+      keep_redo = keep_redo || false;
+      if(!keep_redo) {
+        this.redo_list = [];
+      }
+      
+      (list || this.undo_list).push(canvas.toDataURL());   
+    },
+    undo: function(canvas, tool) {
+      this.restoreState(canvas, tool, this.undo_list, this.redo_list);
+    },
+    redo: function(canvas, tool) {
+      this.restoreState(canvas, tool, this.redo_list, this.undo_list);
+    },
+    restoreState: function(canvas, tool,  pop, push) {
+      if(pop.length) {
+        this.saveState(canvas, push, true);
+        var restore_state = pop.pop();
+        var img = new Element('img', {'src':restore_state});
+        img.onload = function() {
+          tool.clearRect(0, 0, 600, 400);
+          tool.drawImage(img, 0, 0, 600, 400, 0, 0, 600, 400);  
+        }
+      }
+    }
+  }
+  
+
+
